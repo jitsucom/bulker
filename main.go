f4e466a97d37b1da7731e0ebd04a53542a23d5ba
Ildar Nurislamov <absorbb@gmail.com>
2022-08-02 15:03:35 +0300

    Bulker stream interface prototype

--- main.go
+++ main.go
@@ -1,53 +1,80 @@
 package bulker
 
-type StoreResult struct {
+type BulkMode int
+
+const (
+	AutoCommit BulkMode = iota
+	Transactional
+	//DateChunks will work similarly to chunkedStreamSink from js sdk
+	DateChunks
+	//WholeTable atomically replaces target tables similarly to js sdk soruces
+	WholeTable
+)
+
+type Result int
+
+const (
+	Completed Result = iota
+	CompletedWithErrors
+	Aborted
+	Failed
+)
+
+//Granularity is a granularity of TimeInterval
+type Granularity string
+
+const (
+	HOUR    Granularity = "HOUR"
+	DAY     Granularity = "DAY"
+	WEEK    Granularity = "WEEK"
+	MONTH   Granularity = "MONTH"
+	QUARTER Granularity = "QUARTER"
+	YEAR    Granularity = "YEAR"
+	ALL     Granularity = "ALL"
+)
+
+type DateChunksSettings struct {
+	Field       string
+	Granularity Granularity
 }
 
-type StoreContext struct {
-	TableSchema   interface{}
-	Transactional bool
-	WholeTable    bool
-	//Replace
+type Object map[string]interface{}
+
+//StreamsResult is used as a Batch storing result
+type StreamsResult struct {
+	Result         Result
+	LastError      error
+	ProcessedRows  int
+	SuccessfulRows int
+	RowsErrors     map[int]error
 }
+
+type StreamOptions struct {
+	//TODO: Use abstract data types (STRING, BOOL, TIMESTAMP...) or SQL types?
+	TableSchema        interface{}
+	BulkMode           BulkMode
+	DateChunksSettings DateChunksSettings
+	IgnoreErrors       bool
+}
+
+type StreamOption func(*StreamOptions)
 
 type Bulker interface {
-	// do we need such method that also do routing to tables?
-	//Store(objects []map[string]interface{}) (map[string]*StoreResult, error)
-	//TODO: Streaming interface.
-	//TODO: Use chunks in whole table.
-	//TODO: meta type for map[string]interface{}
-	StoreTable(tableName string, storeContext StoreContext, objects []map[string]interface{}) (map[string]*StoreResult, error)
-
-	//ReplaceTable(originalTable, replacementTable string, dropOldTable bool) error
-	//TruncateTable(tableName string) error
+	StartStream(tableName string, streamOptions StreamOptions) (*BulkerStream, error)
 	DropTable(tableName string) error
+
+	//TODO: consider dedicated methods for stream types (to avoid mess with incompatible Options combinations)
+	StartDateChunksStream(tableName string, dateField string, granularity Granularity, streamOptions ...StreamOptions) (*BulkerStream, error)
+	StartWholeTableStream(tableName string, streamOptions ...StreamOptions) (*BulkerStream, error)
+	StartTransactionalStream(tableName string, streamOptions ...StreamOptions) (*BulkerStream, error)
+	StartAutoCommitStream(tableName string, streamOptions ...StreamOptions) (*BulkerStream, error)
+}
+
+type BulkerStream interface {
+	Consume(object Object) error
+	Abort() StreamsResult
+	Close() StreamsResult
 }
 
 //TODO: cleanup deprecated tables
-
 //TODO: no primary keys and similar rows
-//
-//type Storage interface {
-//	io.Closer
-//	DryRun(payload events.Event) ([][]adapters.TableField, error)
-//	Store(fileName string, objects []map[string]interface{}, alreadyUploadedTables map[string]bool, needCopyEvent bool) (map[string]*StoreResult, *events.FailedEvents, *events.SkippedEvents, error)
-//	SyncStore(overriddenDataSchema *schema.BatchHeader, objects []map[string]interface{}, deleteConditions *base.DeleteConditions, cacheTable bool, needCopyEvent bool) error
-//	storeTable(fdata *schema.ProcessedFile) (*adapters.Table, error)
-//	ReplaceTable(originalTable, replacementTable string, dropOldTable bool) error
-//	DropTable(tableName string) error
-//
-//	//Update(object map[string]interface{}) error
-//	Fallback(events ...*events.FailedEvent)
-//	GetUsersRecognition() *UserRecognitionConfiguration
-//	GetSyncWorker() *SyncWorker
-//	GetUniqueIDField() *identifiers.UniqueID
-//	getAdapters() (adapters.SQLAdapter, *TableHelper)
-//	Processor() *schema.Processor
-//	Init(config *Config, impl Storage, preinstalledJavaScript string, defaultUserTransform string) error
-//	Start(config *Config) error
-//	ID() string
-//	Type() string
-//	IsStaging() bool
-//	IsCachingDisabled() bool
-//	Clean(tableName string) error
-//}
