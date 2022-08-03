24f03418a3c1b276a07b8e72b9a11c6a6db822e0
Ildar Nurislamov <absorbb@gmail.com>
2022-08-03 17:36:13 +0300

    BulkLoader helpers

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ type BulkMode int
 const (
 	AutoCommit BulkMode = iota
 	Transactional
-	//DateChunks will work similarly to chunkedStreamSink from js sdk
-	DateChunks
-	//WholeTable atomically replaces target tables similarly to js sdk soruces
+	//DatePartitions will work similarly to chunkedStreamSink from js sdk
+	DatePartitions
+	//WholeTable atomically replaces target tables similarly to js sdk sources
 	WholeTable
 )
 
@@ -19,6 +19,95 @@ const (
 	Aborted
 	Failed
 )
+
+type Object map[string]interface{}
+
+//BulkResult is used as a Batch storing result
+type BulkResult struct {
+	Result         Result
+	LastError      error
+	ProcessedRows  int
+	SuccessfulRows int
+	RowsErrors     map[int]error
+}
+
+type Bulker interface {
+	StartStream(tableName string, mode BulkMode, streamOptions ...StreamOption) (BulkerStream, error)
+}
+
+type BulkerStream interface {
+	Consume(object Object) error
+	Abort() BulkResult
+	Close() BulkResult
+}
+
+type BulkLoader struct {
+	bulker  Bulker
+	mode    BulkMode
+	options []StreamOption
+}
+
+func NewAutoCommitLoader(bulker Bulker, options ...StreamOption) *BulkLoader {
+	bulkLoader := &BulkLoader{
+		bulker:  bulker,
+		options: options,
+		mode:    AutoCommit,
+	}
+	return bulkLoader
+}
+
+func NewTransactionalLoader(bulker Bulker, options ...StreamOption) *BulkLoader {
+	bulkLoader := &BulkLoader{
+		bulker:  bulker,
+		options: options,
+		mode:    Transactional,
+	}
+	return bulkLoader
+}
+
+func NewWholeTableLoader(bulker Bulker, options ...StreamOption) *BulkLoader {
+	bulkLoader := &BulkLoader{
+		bulker:  bulker,
+		options: options,
+		mode:    WholeTable,
+	}
+	return bulkLoader
+}
+
+func NewDatePartitionsLoader(bulker Bulker, datePartitionField string, granularity Granularity, options ...StreamOption) *BulkLoader {
+	bulkLoader := &BulkLoader{
+		bulker:  bulker,
+		options: append(options, WithDatePartitions(datePartitionField, granularity)),
+		mode:    DatePartitions,
+	}
+	return bulkLoader
+}
+
+func (b *BulkLoader) Load(tableName string, objects []Object) (*BulkResult, error) {
+	return b.load(tableName, objects)
+}
+
+func (b *BulkLoader) LoadWithSchema(table interface{}, objects []Object) (*BulkResult, error) {
+	//TODO: replace "tableName" with table.Name
+	return b.load("tableName", objects, WithTable(table))
+}
+
+func (b *BulkLoader) load(tableName string, objects []Object, options ...StreamOption) (*BulkResult, error) {
+	stream, err := b.bulker.StartStream(tableName, b.mode, append(b.options, options...)...)
+	if err != nil {
+		return nil, err
+	}
+	for _, object := range objects {
+		err := stream.Consume(object)
+		if err != nil {
+			res := stream.Close()
+			return &res, err
+		}
+
+	}
+	res := stream.Close()
+	return &res, nil
+}
 
 //Granularity is a granularity of TimeInterval
 type Granularity string
@@ -33,48 +122,40 @@ const (
 	ALL     Granularity = "ALL"
 )
 
-type DateChunksSettings struct {
+type DatePartitionsSettings struct {
 	Field       string
 	Granularity Granularity
 }
 
-type Object map[string]interface{}
+type streamOptions struct {
+	Table interface{}
 
-//StreamsResult is used as a Batch storing result
-type StreamsResult struct {
-	Result         Result
-	LastError      error
-	ProcessedRows  int
-	SuccessfulRows int
-	RowsErrors     map[int]error
+	//TODO: Chunk => Partition. Use not date. Set chunk id.
+	DatePartitionsSettings DatePartitionsSettings
+	IgnoreErrors           bool
 }
 
-type StreamOptions struct {
-	//TODO: Use abstract data types (STRING, BOOL, TIMESTAMP...) or SQL types?
-	TableSchema        interface{}
-	BulkMode           BulkMode
-	DateChunksSettings DateChunksSettings
-	IgnoreErrors       bool
+type StreamOption func(*streamOptions)
+
+func WithTable(table interface{}) StreamOption {
+	return func(options *streamOptions) {
+		options.Table = table
+	}
 }
 
-type StreamOption func(*StreamOptions)
-
-type Bulker interface {
-	StartStream(tableName string, streamOptions StreamOptions) (*BulkerStream, error)
-	DropTable(tableName string) error
-
-	//TODO: consider dedicated methods for stream types (to avoid mess with incompatible Options combinations)
-	StartDateChunksStream(tableName string, dateField string, granularity Granularity, streamOptions ...StreamOptions) (*BulkerStream, error)
-	StartWholeTableStream(tableName string, streamOptions ...StreamOptions) (*BulkerStream, error)
-	StartTransactionalStream(tableName string, streamOptions ...StreamOptions) (*BulkerStream, error)
-	StartAutoCommitStream(tableName string, streamOptions ...StreamOptions) (*BulkerStream, error)
+func WithIgnoreErrors() StreamOption {
+	return func(options *streamOptions) {
+		options.IgnoreErrors = true
+	}
 }
 
-type BulkerStream interface {
-	Consume(object Object) error
-	Abort() StreamsResult
-	Close() StreamsResult
+func WithDatePartitions(datePartitionField string, granularity Granularity) StreamOption {
+	return func(options *streamOptions) {
+		options.DatePartitionsSettings = DatePartitionsSettings{
+			Field:       datePartitionField,
+			Granularity: granularity,
+		}
+	}
 }
 
-//TODO: cleanup deprecated tables
 //TODO: no primary keys and similar rows
