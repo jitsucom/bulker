582b53882b13a34b27816c665388d8f7fc9df737
Ildar Nurislamov <absorbb@gmail.com>
2023-12-04 10:28:16 +0400

    bulker: bigquery: Load: enable PreserveASCIIControlCharacters

--- bulkerlib/implementations/sql/bigquery.go
+++ bulkerlib/implementations/sql/bigquery.go
@@ -586,6 +586,7 @@ func (bq *BigQuery) LoadTable(ctx context.Context, targetTable *Table, loadSourc
 		source.SourceFormat = bigquery.CSV
 		source.SkipLeadingRows = 1
 		source.AllowQuotedNewlines = true
+		source.PreserveASCIIControlCharacters = true
 		source.CSVOptions.NullMarker = "\\N"
 	} else if loadSource.Format == types2.FileFormatNDJSON {
 		source.SourceFormat = bigquery.JSON
