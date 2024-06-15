0cd05b4d684bf67ef6aec525616a6ae899042ea5
Ildar Nurislamov <absorbb@gmail.com>
2024-06-15 11:10:02 +0400

    ingest: some debug info

--- ingest/router_segment_settings_handler.go
+++ ingest/router_segment_settings_handler.go
@@ -3,11 +3,27 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/jitsucom/bulker/jitsubase/logging"
+	"github.com/jitsucom/bulker/jitsubase/types"
 	"net/http"
+	"strings"
+	"time"
 )
+
+var ids = types.NewSet[string]()
+
+func init() {
+	ticker := time.NewTicker(1 * time.Minute)
+	go func() {
+		for range ticker.C {
+			logging.Infof("[SETTINGS]%s", strings.Join(ids.ToSlice(), ","))
+		}
+	}()
+}
 
 func (r *Router) SettingsHandler(c *gin.Context) {
 	writeKey := c.Param("writeKey")
+	ids.Put(strings.SplitN(writeKey, ":", 2)[0])
 	c.Header("Cache-Control", "public, max-age=86400")
 	c.Data(http.StatusOK, "application/json", []byte(fmt.Sprintf(`{
   "integrations": {
