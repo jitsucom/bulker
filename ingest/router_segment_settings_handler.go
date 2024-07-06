d665c05e4e1cf29f1b5820ae44c22edaf3d29505
Ildar Nurislamov <absorbb@gmail.com>
2024-07-06 16:22:58 +0400

    ingest: reduce payload of settings response ingest & bulker: get rid of timeout handler

--- ingest/router_segment_settings_handler.go
+++ ingest/router_segment_settings_handler.go
@@ -10,7 +10,7 @@ import (
 )
 
 const settingsHeader = "{\"integrations\":{\"Segment.io\":{\"apiKey\":\""
-const settingsFooter = "\",\"unbundledIntegrations\":[],\"addBundledMetadata\":true,\"maybeBundledConfigIds\":{},\"versionSettings\":{\"version\":\"4.4.7\",\"componentTypes\":[\"browser\"]}}},\"plan\":{\"track\":{\"__default\":{\"enabled\":true,\"integrations\":{}}},\"identify\":{\"__default\":{\"enabled\":true}},\"group\":{\"__default\":{\"enabled\":true}}},\"edgeFunction\":{},\"analyticsNextEnabled\":true,\"middlewareSettings\":{},\"enabledMiddleware\":{},\"metrics\":{\"sampleRate\":0.1},\"legacyVideoPluginsEnabled\":false,\"remotePlugins\":[]}"
+const settingsFooter = "\",\"versionSettings\":{\"version\":\"4.4.7\",\"componentTypes\":[\"browser\"]}}},\"plan\":{\"track\":{\"__default\":{\"enabled\":true}},\"identify\":{\"__default\":{\"enabled\":true}},\"group\":{\"__default\":{\"enabled\":true}}},\"analyticsNextEnabled\":true}"
 
 var ids = sync.Map{}
 
