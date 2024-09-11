ce7cf6f5218ea7345a12e3ac459d1c7fd2d39c99
Ildar Nurislamov <absorbb@gmail.com>
2024-09-11 09:48:56 +0400

    bulker: fix

--- bulkerlib/implementations/sql/abstract.go
+++ bulkerlib/implementations/sql/abstract.go
@@ -208,7 +208,7 @@ func (ps *AbstractSQLStream) adjustTableColumnTypes(currentTable, existingTable,
 		if existingCol.DataType == newCol.DataType {
 			continue
 		}
-		if newCol.Override || newCol.Important {
+		if (newCol.Override || newCol.Important) && existingCol.New {
 			//if column sql type is overridden by user - leave it this way
 			current.Set(name, newCol)
 			continue
