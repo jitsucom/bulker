0ebe7d65408a2d34989271e0707a26856fb1eca6
Ildar Nurislamov <absorbb@gmail.com>
2025-01-29 20:37:02 +0400

    small fix

--- bulkerlib/implementations/s3.go
+++ bulkerlib/implementations/s3.go
@@ -174,7 +174,7 @@ func (a *S3) GetObjectURL(fileName string) (string, error) {
 				logging.Errorf("[%s] Error presigning URL: %v", a.Type(), err)
 				return "", nil
 			}
-			logging.Infof("Presigned URL: %s + %s", request.URL, time.Since(startedAt))
+			logging.Infof("Presigned URL for %s: %s", fileName, time.Since(startedAt))
 			return request.URL, nil
 		} else {
 			return fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", a.config.Bucket, a.config.Region, fileName), nil
