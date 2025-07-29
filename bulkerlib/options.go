5945ec402b585491db0a57ef2d73db93d1ffd8df
Ildar Nurislamov <absorbb@gmail.com>
2025-07-29 16:43:24 +0400

    bulker: added "disabled" to known options

--- bulkerlib/options.go
+++ bulkerlib/options.go
@@ -14,7 +14,7 @@ type StreamOption func(*StreamOptions)
 var optionsRegistry = make(map[string]ParseableOption)
 
 // Not used by bulker. Just added here to be treated as known options and don't print errors
-var ignoredOptions = []string{"functions", "streams", "dataLayout", "events", "debugTill", "hosts", "schedule", "timezone", "storageKey", "tableNamePrefix", "multithreading", "keepOriginalNames", "addMeta"}
+var ignoredOptions = []string{"functions", "streams", "dataLayout", "events", "debugTill", "hosts", "schedule", "timezone", "storageKey", "tableNamePrefix", "multithreading", "keepOriginalNames", "addMeta", "disabled"}
 
 var (
 	BatchSizeOption = ImplementationOption[int]{
