73b98bd22aa4447a26c76ef80876f2fda6efdd22
Ildar Nurislamov <absorbb@gmail.com>
2025-10-16 14:08:23 +0400

    fix(bulker): upgraded duckdb driver

--- admin/main.go
+++ admin/main.go
@@ -1,16 +1,18 @@
 package main
 
+import (
+	"github.com/jitsucom/bulker/jitsubase/appbase"
+	"os"
+)
+
 func main() {
-	//DeleteEmptyTopics()
-	mongoProfile()
-	//mongoStore()
-	//settings := &appbase.AppSettings{
-	//	ConfigPath: os.Getenv("ADMIN_CONFIG_PATH"),
-	//	Name:       "admin",
-	//	EnvPrefix:  "ADMIN",
-	//	ConfigName: "admin",
-	//	ConfigType: "env",
-	//}
-	//application := appbase.NewApp[Config](&Context{}, settings)
-	//application.Run()
+	settings := &appbase.AppSettings{
+		ConfigPath: os.Getenv("ADMIN_CONFIG_PATH"),
+		Name:       "admin",
+		EnvPrefix:  "ADMIN",
+		ConfigName: "admin",
+		ConfigType: "env",
+	}
+	application := appbase.NewApp[Config](&Context{}, settings)
+	application.Run()
 }
